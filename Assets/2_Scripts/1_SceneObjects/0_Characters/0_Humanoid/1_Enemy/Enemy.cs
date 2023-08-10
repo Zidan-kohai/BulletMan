eb3e94d166b8eb16f81a69adbdf25613b9ca6940
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2023-08-10 06:53:51 +0300

    [few]
    [add] Enemy animation
    [add] Enemy animation interactive with Ragdoll

--- Assets/2_Scripts/1_SceneObjects/0_Characters/0_Humanoid/1_Enemy/Enemy.cs
+++ Assets/2_Scripts/1_SceneObjects/0_Characters/0_Humanoid/1_Enemy/Enemy.cs
@@ -1,10 +1,25 @@
 using DG.Tweening;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.IK;
 
 public class Enemy : MonoBehaviour, IDinamicObject
 {
     [Header("General")]
     [SerializeField] private float waitToReady;
+
+    [Header("Components")]
+    [SerializeField] private Animator anim;
+    [SerializeField] private List<Solver2D> solvers;
+    [SerializeField] private List<Rigidbody2D> rigidbodies;
+    [SerializeField] private List<HingeJoint2D> hingleJoints;
+    [SerializeField] private List<Collider2D> colliders;
+
+
+    [Header("Collision")]
+    [SerializeField] private List<IgnoreCollistion> ignoreCollision;
+
     private void OnEnable()
     {
         OnSpawn(waitToReady);
@@ -16,7 +31,23 @@ public class Enemy : MonoBehaviour, IDinamicObject
         {
             GameLoopSystem.Instance.GetSystem<GameEventSystem>().OnSpawnObject(this);
 
+            IgnoreCollision();
+
         }).SetLink(gameObject);
+    }
+
+    private void IgnoreCollision()
+    {
+        foreach (var item in ignoreCollision)
+        {
+            for (int i = 0; i < item.Collisions.Count; i++)
+            {
+                for (int j = i + 1; j < item.Collisions.Count; j++)
+                {
+                    Physics2D.IgnoreCollision(item.Collisions[i], item.Collisions[j]);
+                }
+            }
+        }
     }
 
     public void EveryFrame()
@@ -24,7 +55,36 @@ public class Enemy : MonoBehaviour, IDinamicObject
 
     }
 
+    protected virtual void GetDamage()
+    {
+        Death();
+    }
+
+    [ContextMenu("Death")]
+    private void Death()
+    {
+        anim.enabled = false;
+
+        foreach(var s in solvers)
+            s.weight = 0f;
+
+        foreach(var r in rigidbodies)
+            r.simulated = true;
+
+        foreach (var h in hingleJoints)
+            h.enabled = true;
+
+        foreach (var c in colliders)
+            c.isTrigger = false;
+    }
+
     private void OnDestroy()
     {
+
     }
+}
+[Serializable]
+public struct IgnoreCollistion
+{
+    public List<Collider2D> Collisions;
 }
